15c7b8cb799826600ecf53efdb04bd807550b5ae
bee <rkddbtjd123@gmail.com>
2021-12-15 01:17:11 +0900

    feat: UserServiceImpl 서비스 어노테이션 추가

--- session/src/main/java/me/spring/security/user/UserServiceImpl.java
+++ session/src/main/java/me/spring/security/user/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package me.spring.security.user;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl implements UserService {
     @Override
     public void registerUser(RegisterUserRequest registerUserRequest) {
