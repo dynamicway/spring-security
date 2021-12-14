3d8a5d06bd0326a0e02b0817293e492b274a6f16
bee <rkddbtjd123@gmail.com>
2021-12-14 17:30:56 +0900

    build: 멀티 모듈 프로젝트로 변경

--- components/component-session/src/main/java/me/spring/security/user/UserApi.java
+++ components/component-session/src/main/java/me/spring/security/user/UserApi.java
@@ -1,6 +1,6 @@
 package me.spring.security.user;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +9,7 @@ import static org.springframework.http.HttpStatus.CREATED;
 @RestController
 public class UserApi {
 
-    @PostMapping("/users")
+    @GetMapping("/users")
     @ResponseStatus(CREATED)
     public void registerUser() {
 
