d43888fd331292231d23116d09305ad696880a46
bee <rkddbtjd123@gmail.com>
2021-12-18 00:22:01 +0900

    test: UserEntity 에 UserRole 추가

--- session/src/test/java/me/spring/security/config/UserDetailsServiceTest.java
+++ session/src/test/java/me/spring/security/config/UserDetailsServiceTest.java
@@ -1,10 +1,15 @@
 package me.spring.security.config;
 
+import me.spring.security.error.NotFoundEntityException;
 import me.spring.security.user.SpyUserRepository;
+import me.spring.security.user.UserEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserDetailsServiceTest {
 
@@ -22,7 +27,18 @@ class UserDetailsServiceTest {
     @Test
     void loadUserByUsername_callsFindByName_inUserRepository() {
         String givenUsername = "userName";
+        spyUserRepository.findByName_returns = Optional.of(new UserEntity());
         userDetailsService.loadUserByUsername(givenUsername);
         assertThat(spyUserRepository.findByName_arguments).isEqualTo(givenUsername);
     }
+
+    @Test
+    void loadUserByUsername_throwsNotFoundEntityException_when_notFoundUser() {
+        String givenNotExistsUserName = "userName";
+        spyUserRepository.findByName_returns = Optional.empty();
+        assertThatThrownBy(() -> userDetailsService.loadUserByUsername(givenNotExistsUserName))
+                .isInstanceOf(NotFoundEntityException.class)
+                .hasMessage("not exists user");
+    }
+
 }
