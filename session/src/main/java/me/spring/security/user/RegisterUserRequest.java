8d00ba544a05ffada1bdb0ac486b2ce13dde2e3e
bee <rkddbtjd123@gmail.com>
2021-12-19 22:00:43 +0900

    feat: RegisterUserRequest::toEntity 메서드에서 UserRole 도 정상적으로 변환되도록 수정

--- session/src/main/java/me/spring/security/user/RegisterUserRequest.java
+++ session/src/main/java/me/spring/security/user/RegisterUserRequest.java
@@ -1,9 +1,12 @@
 package me.spring.security.user;
 
 import lombok.*;
+import me.spring.security.error.BadRequestException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static lombok.AccessLevel.*;
 
@@ -18,10 +21,19 @@ public class RegisterUserRequest {
     private final List<UserRole.Role> roles = new ArrayList<>();
 
     public UserEntity toEntity() {
-        return new UserEntity(
+        validate();
+        UserEntity userEntity = new UserEntity(
                 name,
                 password,
                 email
         );
+        Set<UserRole> userRoles = roles.stream().map(role -> new UserRole(userEntity, role)).collect(Collectors.toSet());
+        userEntity.getRoles().addAll(userRoles);
+        return userEntity;
+    }
+
+    private void validate() {
+        if (roles.isEmpty())
+            throw new BadRequestException("roles is empty");
     }
 }
--- session/src/test/java/me/spring/security/user/RegisterUserRequestTest.java
+++ session/src/test/java/me/spring/security/user/RegisterUserRequestTest.java
@@ -1,8 +1,10 @@
 package me.spring.security.user;
 
+import me.spring.security.error.BadRequestException;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RegisterUserRequestTest {
 
@@ -12,9 +14,26 @@ class RegisterUserRequestTest {
                 "name",
                 "password",
                 "email");
+        registerUserRequest.getRoles().add(UserRole.Role.USER);
         UserEntity toEntity_returns = registerUserRequest.toEntity();
         assertThat(toEntity_returns.getName()).isEqualTo("name");
         assertThat(toEntity_returns.getPassword()).isEqualTo("password");
         assertThat(toEntity_returns.getEmail()).isEqualTo("email");
+        assertThat(toEntity_returns.getRoles()).containsExactly(
+                new UserRole(toEntity_returns, UserRole.Role.USER)
+        );
+
+    }
+
+    @Test
+    void toEntity_throwsBadRequestException_when_rolesIsEmpty() {
+        RegisterUserRequest registerUserRequest = new RegisterUserRequest(
+                "name",
+                "password",
+                "email");
+
+        assertThatThrownBy(registerUserRequest::toEntity)
+                .isInstanceOf(BadRequestException.class)
+                .hasMessage("roles is empty");
     }
 }
