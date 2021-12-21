d7447b80804bc7c4cafb858e6f4751f68564b4a7
bee <rkddbtjd123@gmail.com>
2021-12-21 19:25:34 +0900

    feat: UserAuthenticationToken 에 Authentication 인터페이스 구현

--- token/src/main/java/me/spring/security/security/UserAuthenticationToken.java
+++ token/src/main/java/me/spring/security/security/UserAuthenticationToken.java
@@ -2,12 +2,49 @@ package me.spring.security.security;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
-import static lombok.AccessLevel.PRIVATE;
+import java.util.Collection;
 
 @Getter
-@NoArgsConstructor(access = PRIVATE)
-public class UserAuthenticationToken {
+@NoArgsConstructor
+public class UserAuthenticationToken implements Authentication {
     private String email;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return false;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
 }
