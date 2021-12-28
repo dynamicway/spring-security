4bcecb430c4e3496b934b432bfd18386e8c2a573
bee <rkddbtjd123@gmail.com>
2021-12-28 19:21:14 +0900

    test: UserAuthenticationSuccessHandler::onAuthenticationSuccess 메서드에서 JwtTokenManager::generateJwt 메서드 호출 검증 테스트 추가

--- token/src/test/java/me/spring/security/security/UserAuthenticationSuccessHandlerTest.java
+++ token/src/test/java/me/spring/security/security/UserAuthenticationSuccessHandlerTest.java
@@ -1,6 +1,30 @@
 package me.spring.security.security;
 
+import me.spring.security.security.testdouble.SpyJwtTokenManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class UserAuthenticationSuccessHandlerTest {
 
+    private UserAuthenticationSuccessHandler userAuthenticationSuccessHandler;
+    private SpyJwtTokenManager spyJwtTokenManager;
+
+    @BeforeEach
+    void setUp() {
+        spyJwtTokenManager = new SpyJwtTokenManager();
+        userAuthenticationSuccessHandler = new UserAuthenticationSuccessHandler(spyJwtTokenManager);
+    }
+
+    @Test
+    void onAuthenticationSuccess_callsGenerateJwt_inJwtTokenManager() {
+        MockHttpServletRequest givenHttpRequest = new MockHttpServletRequest();
+        UserAuthenticationToken givenUserAuthenticationToken = new UserAuthenticationToken();
+        userAuthenticationSuccessHandler.onAuthenticationSuccess(givenHttpRequest, new MockHttpServletResponse(), givenUserAuthenticationToken);
+        assertThat(spyJwtTokenManager.generateJwt_arguments).isEqualTo(givenUserAuthenticationToken);
+    }
 
 }
