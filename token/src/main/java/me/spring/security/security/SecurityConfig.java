a49dfb3031c004e5d817db0572176bcfaa83ba8b
bee <rkddbtjd123@gmail.com>
2021-12-28 23:20:51 +0900

    feat: SecurityConfig 에 UserAuthenticationFilter 등록

--- token/src/main/java/me/spring/security/security/SecurityConfig.java
+++ token/src/main/java/me/spring/security/security/SecurityConfig.java
@@ -1,12 +1,17 @@
 package me.spring.security.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserAuthenticationFilter userAuthenticationFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -14,7 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .formLogin()
-                .disable();
+                .disable()
+                .addFilterBefore(userAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
 }
