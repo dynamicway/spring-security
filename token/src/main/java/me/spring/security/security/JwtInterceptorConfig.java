c30d3cc42193da0d0596bd4eac0fb734d0034eef
bee <rkddbtjd123@gmail.com>
2021-12-29 16:41:26 +0900

    feat: JwtInterceptorConfig 추가

---
+++ token/src/main/java/me/spring/security/security/JwtInterceptorConfig.java
@@ -0,0 +1,20 @@
+package me.spring.security.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class JwtInterceptorConfig implements WebMvcConfigurer {
+
+    private final JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+                .addPathPatterns("/user");
+    }
+
+}
