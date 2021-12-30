2cec8c64fdbcba9588777b4021c3d5c34461108a
bee <rkddbtjd123@gmail.com>
2021-12-31 00:41:45 +0900

    feat: OAuth2SecurityConfig 추가

---
+++ oauth2/src/main/java/me/spring/security/security/OAuth2SecurityConfig.java
@@ -0,0 +1,20 @@
+package me.spring.security.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class OAuth2SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin()
+                .disable();
+    }
+
+}
