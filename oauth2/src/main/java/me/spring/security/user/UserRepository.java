90819b5baaf8631077163cd198861278072b4a58
bee <rkddbtjd123@gmail.com>
2021-12-31 16:04:04 +0900

    feat: UserRepository::findByResourceServerAndResourceServerId 추가

--- oauth2/src/main/java/me/spring/security/user/UserRepository.java
+++ oauth2/src/main/java/me/spring/security/user/UserRepository.java
@@ -1,6 +1,12 @@
 package me.spring.security.user;
 
+import me.spring.security.security.oauth.ResourceServer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByResourceServerAndResourceServerId(ResourceServer resourceServer, long resourceServerId);
+
 }
