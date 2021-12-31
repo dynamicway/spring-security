6cfb083382b4cfb2ad1d8883a51bcce392a8c8fa
bee <rkddbtjd123@gmail.com>
2021-12-31 18:26:31 +0900

    feat: ResourceServer.nameAttribute, thumbnail 필드 추가

--- oauth2/src/main/java/me/spring/security/security/oauth/ResourceServer.java
+++ oauth2/src/main/java/me/spring/security/security/oauth/ResourceServer.java
@@ -1,7 +1,17 @@
 package me.spring.security.security.oauth;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ResourceServer {
-    KAKAO,
-    NAVER,
-    GOOGLE
+
+    KAKAO("id", "profile_image"),
+    NAVER("id", null),
+    GOOGLE("sub", null);
+
+    private final String nameAttribute;
+    private final String thumbnail;
+
 }
