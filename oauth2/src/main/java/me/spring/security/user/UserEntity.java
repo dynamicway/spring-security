ffc748e256373d5c5273d9c4e45ca200e58540f9
bee <rkddbtjd123@gmail.com>
2021-12-31 15:55:17 +0900

    feat: UserEntity 초기화

---
+++ oauth2/src/main/java/me/spring/security/user/UserEntity.java
@@ -0,0 +1,32 @@
+package me.spring.security.user;
+
+import lombok.Getter;
+import me.spring.security.security.oauth.ResourceServer;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.EnumType.*;
+
+@Entity
+@Getter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "nick_name")
+    private String nickName;
+    private LocalDate birth;
+
+    @Column(name = "resource_server_name")
+    @Enumerated(STRING)
+    private ResourceServer resourceServer;
+
+    @OneToMany
+    private final List<UserRoleEntity> roles = new ArrayList<>();
+
+}
