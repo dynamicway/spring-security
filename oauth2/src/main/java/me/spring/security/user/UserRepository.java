5ea7628a4dd904311b8a199ab3172b8aba651def
bee <rkddbtjd123@gmail.com>
2021-12-31 17:24:51 +0900

    feat: User 관련 entity 수정

--- oauth2/src/main/java/me/spring/security/user/UserRepository.java
+++ oauth2/src/main/java/me/spring/security/user/UserRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    Optional<UserEntity> findByResourceServerAndResourceServerId(ResourceServer resourceServer, long resourceServerId);
+    Optional<UserEntity> findByResourceServerAndResourceServerId(ResourceServer resourceServer, String resourceServerId);
 
 }
