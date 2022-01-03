2ef1bae8a960a0e9dc21526e31f79db182412590
bee <rkddbtjd123@gmail.com>
2022-01-03 13:02:47 +0900

    feat: OAuth2SecurityConfig 에 CustomOAuth2UserService, OAuth2LoginSuccessHandler 추가

--- oauth2/src/main/java/me/spring/security/security/OAuth2SecurityConfig.java
+++ oauth2/src/main/java/me/spring/security/security/OAuth2SecurityConfig.java
@@ -1,12 +1,19 @@
 package me.spring.security.security;
 
+import lombok.RequiredArgsConstructor;
+import me.spring.security.security.oauth.CustomOAuth2UserService;
+import me.spring.security.security.oauth.OAuth2LoginSuccessHandler;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class OAuth2SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -14,7 +21,10 @@ public class OAuth2SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .formLogin()
-                .disable();
+                .disable()
+                .oauth2Login()
+                .successHandler(oAuth2LoginSuccessHandler)
+                .userInfoEndpoint().userService(customOAuth2UserService);
     }
 
 }
