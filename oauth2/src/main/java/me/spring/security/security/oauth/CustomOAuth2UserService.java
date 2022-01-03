92a4ea79647c53579a59038ebd56c0277a2d3888
bee <rkddbtjd123@gmail.com>
2022-01-03 13:01:01 +0900

    feat: CustomOAuth2UserService::loadUser 에 Transactional 어노테이션 추가

--- oauth2/src/main/java/me/spring/security/security/oauth/CustomOAuth2UserService.java
+++ oauth2/src/main/java/me/spring/security/security/oauth/CustomOAuth2UserService.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,6 +23,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     private final DefaultOAuth2UserFactory defaultOAuth2UserFactory;
 
     @Override
+    @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(userRequest);
         UserEntity userEntity = getOrSaveUserEntity(userRequest, oAuth2User);
